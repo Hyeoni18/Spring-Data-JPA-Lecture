67dd60476bb522b72a31ff576b4c1a2e340b64db
Hyeoni18 <gusgnl105@gmail.com>
2022-02-18 05:36:24 +0900

    JPA 프로그래밍 2.엔티티 타입 맵핑

--- src/main/java/hello/springboot/Account.java
+++ src/main/java/hello/springboot/Account.java
@@ -1,20 +1,27 @@
 package hello.springboot;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
-@Entity
+@Entity(name = "myAccount") //테이블 이름.
 public class Account {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue //자동생성
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
 
+    @Temporal(TemporalType.DATE)
+    private Date created = new Date();
+
+    @Transient //칼럼으로 매핑하지 않을 때 사용
+    private String yes;
+
+    //getter, setter 없이도 칼럼매핑 가능
     public Long getId() {
         return id;
     }
