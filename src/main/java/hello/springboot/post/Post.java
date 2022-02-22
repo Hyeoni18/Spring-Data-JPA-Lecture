92ebea48898a31df4a116f9790eb556f4863dd40
Hyeoni18 <gusgnl105@gmail.com>
2022-02-23 06:35:43 +0900

    스프링 데이터 Common 9.도메인 이벤트

--- src/main/java/hello/springboot/post/Post.java
+++ src/main/java/hello/springboot/post/Post.java
@@ -1,10 +1,12 @@
 package hello.springboot.post;
 
+import org.springframework.data.domain.AbstractAggregateRoot;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-public class Post {
+public class Post extends AbstractAggregateRoot<Post> {
 
     @Id @GeneratedValue
     private long id;
@@ -47,5 +49,11 @@ public class Post {
 
     public void setCreated(Date created) {
         this.created = created;
+    }
+
+    public Post publish() {
+        //퍼블리시 할 때 여기서 만들면 되는거야
+        this.registerEvent(new PostPublishedEvent(this)); //상속받은 클래스에서 제공되는 메소드. 안에 이벤트를 만들어서 넣어주면 됨. 끝임.
+        return this;
     }
 }
--- src/test/java/hello/springboot/post/PostRepositoryTest.java
+++ src/test/java/hello/springboot/post/PostRepositoryTest.java
@@ -5,27 +5,43 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@Import(PostRepositoryTestConfig.class) //테스트용 추가설정을 import
 public class PostRepositoryTest extends TestCase {
 
     @Autowired
     PostRepository postRepository;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void event() {
+        //퍼블리싱
+        Post post = new Post();
+        post.setTitle("EVENT");
+        PostPublishedEvent event = new PostPublishedEvent(post);
+
+        applicationContext.publishEvent(event);
+    }
 
     @Test
     public void curd() {
         Post post = new Post();
         post.setTitle("Hibernate");
 
-        assertThat(postRepository.contains(post)).isFalse(); //트랜지스트 상태
+        assertThat(postRepository.contains(post)).isFalse();
 
-        postRepository.save(post);
+        postRepository.save(post.publish());
 
-        assertThat(postRepository.contains(post)).isTrue(); //펄시스트 상태
+        assertThat(postRepository.contains(post)).isTrue();
 
         postRepository.delete(post);
         postRepository.flush();
