5e053aadd682cd82595a73a35a149c8f518962c6
Hyeoni18 <gusgnl105@gmail.com>
2022-02-23 20:37:28 +0900

    스프링 데이터 JPA 2.JpaRepository.save() 메소드

--- src/test/java/hello/springboot/post/PostControllerTest.java
+++ src/test/java/hello/springboot/post/PostControllerTest.java
@@ -1,15 +1,17 @@
 package hello.springboot.post;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,60 +20,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@DataJpaTest
 public class PostControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
 
     @Autowired
     PostRepository postRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
-    public void curd() {
+    public void save() {
         Post post = new Post();
         post.setTitle("JPA");
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post); //insert query 발생, persist에 넘겨준 post 인스턴스 자체가 영속화됨.
 
-        List<Post> posts = postRepository.findAll();
+        assertThat(entityManager.contains(post)).isTrue();
+        assertThat(entityManager.contains(savedPost)).isTrue();
+        assertThat(savedPost == post);
+
+        Post postUpdate = new Post();
+        postUpdate.setTitle("JPA222");
+        postUpdate.setId(post.getId());
+        Post updatedPost = postRepository.save(postUpdate); //update query 발생, merge, 복사본을 만들어서 영속화를 하고 복사본은 persist하지 않고 그 자체를 리턴해줌.
+
+        assertThat(entityManager.contains(updatedPost)).isTrue(); //updatedPost 영속화
+        assertThat(entityManager.contains(postUpdate)).isFalse(); //postUpdate 영속화 되지 않음
+        assertThat(updatedPost == postUpdate);
+
+        //그러니까 항상 반환을 받아서 사용하는게 좋아. postUpdate를 다시 사용하게 되면 persist가 감지를 못해.
+        updatedPost.setTitle("do it"); //1.update를 하라고 알려주지 않아도 객체 상태 변화를 감지해서 알아서 update를 해줌
+
+        List<Post> posts = postRepository.findAll(); //2.여기서. 데이터를 가져오려고 하네? 빨리 싱크해야겠다. 이게 persist 상태의 객체임.
         assertThat(posts.size()).isEqualTo(1);
-    }
-
-    @Test
-    public void getPost() throws Exception {
-        Post post = new Post();
-        post.setTitle("JPA");
-        postRepository.save(post);
-
-        mockMvc.perform(get("/posts/"+post.getId()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("JPA"));
-    }
-
-    @Test
-    public void getPosts() throws Exception {
-        createPosts();
-
-        mockMvc.perform(get("/posts/")
-                        .param("page","3") //4번째 페이지 쿼리
-                        .param("size","10") //한 페이지 10개씩
-                        .param("sort","created,desc") //정렬. 가장 최신부터.
-                        .param("sort","title") //날짜가 같으면 다음 정렬 옵션은 타이틀.
-                )
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content[0].title").value("JPA"));
-    }
-
-    private void createPosts() {
-        int postsCount = 100;
-        while(postsCount > 0) {
-            Post post = new Post();
-            post.setTitle("JPA");
-            postRepository.save(post);
-            postsCount--;
-        }
     }
 }
