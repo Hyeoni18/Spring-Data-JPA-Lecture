c178d7cdf9c0916a46ce6a39ce1c7e0891f5b465
Hyeoni18 <gusgnl105@gmail.com>
2022-02-23 17:20:50 +0900

    스프링 데이터 Common 14.웹 기능 4부 HATEOAS

--- src/main/java/hello/springboot/post/PostController.java
+++ src/main/java/hello/springboot/post/PostController.java
@@ -3,6 +3,9 @@ package hello.springboot.post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,7 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public Page<Post> getPosts(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public PagedResources<Resource<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) { //추가한 엔티티 타입의 리소스를 만들겠다는 의미
+        return assembler.toResource(postRepository.findAll(pageable));
     }
 }
--- src/test/java/hello/springboot/post/PostControllerTest.java
+++ src/test/java/hello/springboot/post/PostControllerTest.java
@@ -39,18 +39,26 @@ public class PostControllerTest {
 
     @Test
     public void getPosts() throws Exception {
-        Post post = new Post();
-        post.setTitle("JPA");
-        postRepository.save(post);
+        createPosts();
 
         mockMvc.perform(get("/posts/")
-                        .param("page","0") //page 파라미터는 0부터 시작함. 0이 첫 번째 페이지.
-                        .param("size","10") //한 페이지 몇 개씩 가져올지. 10개 가져올 거임.
+                        .param("page","3") //4번째 페이지 쿼리
+                        .param("size","10") //한 페이지 10개씩
                         .param("sort","created,desc") //정렬. 가장 최신부터.
                         .param("sort","title") //날짜가 같으면 다음 정렬 옵션은 타이틀.
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content[0].title").value("JPA"));
+    }
+
+    private void createPosts() {
+        int postsCount = 100;
+        while(postsCount > 0) {
+            Post post = new Post();
+            post.setTitle("JPA");
+            postRepository.save(post);
+            postsCount--;
+        }
     }
 }
