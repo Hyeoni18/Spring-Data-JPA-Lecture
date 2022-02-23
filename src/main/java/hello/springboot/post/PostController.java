17eafe748c0fc0876a7cf2338f7e81f919fcdde4
Hyeoni18 <gusgnl105@gmail.com>
2022-02-23 17:04:55 +0900

    스프링 데이터 Common 13.웹 기능 3부 Pageable과 Sort

--- src/main/java/hello/springboot/post/PostController.java
+++ src/main/java/hello/springboot/post/PostController.java
@@ -1,6 +1,8 @@
 package hello.springboot.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,13 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
-//    private final PostRepository postRepository;
-//
-//    public PostController(PostRepository postRepository) {
-//        this.postRepository = postRepository;
-//    }
-
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
     }
 }
--- src/test/java/hello/springboot/post/PostControllerTest.java
+++ src/test/java/hello/springboot/post/PostControllerTest.java
@@ -11,9 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,5 +35,22 @@ public class PostControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("JPA"));
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("JPA");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                        .param("page","0") //page 파라미터는 0부터 시작함. 0이 첫 번째 페이지.
+                        .param("size","10") //한 페이지 몇 개씩 가져올지. 10개 가져올 거임.
+                        .param("sort","created,desc") //정렬. 가장 최신부터.
+                        .param("sort","title") //날짜가 같으면 다음 정렬 옵션은 타이틀.
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].title").value("JPA"));
     }
 }
