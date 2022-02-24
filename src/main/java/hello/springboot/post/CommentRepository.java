728dcbfd0f0d531bbaf6c1eb03ab107385a4cf8d
Hyeoni18 <gusgnl105@gmail.com>
2022-02-25 00:46:27 +0900

    스프링 데이터 JPA 11.트랜잭션

--- src/main/java/hello/springboot/post/CommentRepository.java
+++ src/main/java/hello/springboot/post/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
     @EntityGraph(attributePaths = "post")
     Optional<Comment> getById(Long id);
 
+    @Transactional(readOnly = true) //그냥 이렇게 추가해서 사용할 수 있음. 근데 이때는 readonly=true가 좋음. 값을 변경하는게 아니니까.
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 }
